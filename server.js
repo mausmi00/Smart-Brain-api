9989515244467d9ddc13d57153fb53132a0ba3f0
mausmi00 <mausmi000@gmail.com>
2020-05-12 16:52:40 -0700

    updated response

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{res.json('it is working')})
+app.get('/',(req,res)=>{res.send("it is working")})
 app.post('/signin', (req,res) => {signIn.handleSignIn(req, res, db, bcrypt)});
 app.post('/register', (req,res) => {register.handleRegister(req,res, db, bcrypt)});
 app.get('/profile/:id', (req,res) => {profile.handleProfile(req.res.db)});
