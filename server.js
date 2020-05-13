7e8c9e1ad0e752c5c7020e11cb4e72fa1b3585b2
mausmi00 <mausmi000@gmail.com>
2020-05-12 17:19:30 -0700

    adding database

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const db = knex({
     client: 'pg',
     version: '7.2',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'mausmi909',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
+      
     }
   }); 
 const app = express();
